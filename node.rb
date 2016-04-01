1daef2615dccf4fd207bfbdb3ea7804e857916b1
App Academy Student <student@appacademy.io>
2016-04-01 16:34:46 -0700

    node: left and right

--- node.rb
+++ node.rb
@@ -1,10 +1,22 @@
 class BinaryNode
   attr_reader :value
+  attr_accessor :parent
+  
   def initialize(value)
     @value = value
     @parent = nil
     @left = nil
     @right = nil
+  end
+
+  def left=(node)
+    @left = node
+    node.parent = self
+  end
+
+  def right=(node)
+    @right = node
+    node.parent = self
   end
 
 end
